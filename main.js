90e44e8ebd4894b02b21a8e9a3338af96b832411
Luisa Vaca <lvaca@hugeinc.com>
2015-12-03 14:59:48 -0500

    Domo runs

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var timelineOne = new TimelineMax();
 var timelineTwo = new TimelineMax();
 var domo = document.querySelector('.body');
+var footLeft = document.querySelector('.foot-left');
+var footRight = document.querySelector('.foot-right');
 
   timelineOne
-    .to (domo, 0.5, {rotation: 360, transformOrigin: "45px 57px"});
+    .to (domo, 6, {scale: 2});
 
   timelineTwo
-    .to (domo, 0.5, {scale:2});
+    .to (footLeft, 0.3, {top: '104px', repeat:15, yoyo:true})
+    //.to (footLeft, 0.3, {top: '114px'})
+    .to (footRight, 0.3, {top: '104px', repeat:15, yoyo:true}, '0.3')
