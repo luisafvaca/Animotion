45a3c2370cfbd5a1f574386d09016150d8c38efe
Luisa Vaca <lvaca@hugeinc.com>
2015-12-03 15:44:43 -0500

    big eyes

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var timelineTwo = new TimelineMax();
 var domo = document.querySelector('.body');
 var footLeft = document.querySelector('.foot-left');
 var footRight = document.querySelector('.foot-right');
+var handLeft = document.querySelector('.hand-left');
 
   timelineOne
     .to (domo, 6, {scale: 2});
 
   timelineTwo
     .to (footLeft, 0.3, {top: '104px', repeat:15, yoyo:true})
-    //.to (footLeft, 0.3, {top: '114px'})
     .to (footRight, 0.3, {top: '104px', repeat:15, yoyo:true}, '0.3')
+    //.to (handLeft, 0.1, {rotation: 0.5, transformOrigin:" '' 100%",repeat:3, left: '-8px', yoyo:true}, '0.3');
